4fa7049c807244b9975a63786aa53399808174f0
Cody_Jones <cbjones93@gmail.com>
2021-07-13 14:28:06 -0500

    white screen

--- Streamish/Repositories/UserProfileRepository.cs
+++ Streamish/Repositories/UserProfileRepository.cs
@@ -91,6 +91,41 @@ namespace Streamish.Repositories
                 }
             }
         }
+        public UserProfile GetByFirebaseUserId(string firebaseUserId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT up.Id, Up.FirebaseUserId, up.Name AS UserProfileName, up.Email, up.DateCreated AS UserProfileDateCreated
+                            
+                          FROM UserProfile up
+                         WHERE FirebaseUserId = @FirebaseuserId";
+
+                    DbUtils.AddParameter(cmd, "@FirebaseUserId", firebaseUserId);
+
+                    UserProfile userProfile = null;
+
+                    var reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        userProfile = new UserProfile()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            FirebaseUserId = DbUtils.GetString(reader, "FirebaseUserId"),
+                            Name = DbUtils.GetString(reader, "UserProfileName"),
+                            Email = DbUtils.GetString(reader, "Email"),
+                            DateCreated = DbUtils.GetDateTime(reader, "DateCreated")
+                        };
+                    }
+                    reader.Close();
+
+                    return userProfile;
+                }
+            }
+        }
         public UserProfile GetById(int id)
         {
             using (var conn = Connection)
